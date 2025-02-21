f0a5ce92572b0ecd3b3ab394779bb0b71bb34023
kateroook <severinakat11@gmail.com>
2025-02-21 22:14:54 +0100

    init

---
+++ frontend/src/components/FileManager.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import FileUpload from "./FileUpload";
+import FilesList from "./FilesList";
+import axios from "axios";
+import {Box, Divider, Paper} from "@mui/material";
+
+const FileManager = () => {
+    const [files, setFiles] = useState([]);
+    const fetchFiles = async () => {
+        try {
+            const response = await axios.get("http://localhost:5000/api/files", { withCredentials: true });
+            setFiles(response.data);
+        } catch (error) {
+            console.error("Error fetching files:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchFiles();
+    }, []);
+
+    const handleFileUploaded = (newFile) => {
+        setFiles((prevFiles) => {
+            const updatedFiles = [...prevFiles, newFile];
+            // console.log("Updated Files List:", updatedFiles);
+            return updatedFiles;
+        });
+    };
+    const handleFileUpdated = () => {
+        fetchFiles(); // Refresh file list
+    };
+
+
+    return (
+        <Box flex={1} display="flex" flexDirection="column" gap={2}>
+            {/* Subjects Section */}
+            <Paper elevation={3} sx={{ flex: 2, padding: 2, borderRadius: 4, display: 'flex', justifyContent:'space-between', alignContent: 'space-between'}}>
+                <FilesList files={files} onFileUpdated={handleFileUpdated} />
+            </Paper>
+
+            {/* Upload Section */}
+                <FileUpload onFileUploaded={handleFileUploaded} />
+
+        </Box>
+    );
+};
+
+export default FileManager;
